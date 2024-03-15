b00f273462447e74d65386885cf27f458e3047dc
Tilen Tratnjek <80243427+Tilchy@users.noreply.github.com>
2024-03-15 07:51:47 +0100

    Updated Folder structure

--- src/main/java/com/tratnjek/assetmanagmentsystem/AssetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tratnjek.assetmanagmentsystem;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface AssetRepository extends CrudRepository<Asset,Long> {
-}
